dee5d6a96c3e19a265b2d673e8c2c0fb579323ef
RemiJonathan <contact@remijonathan.com>
2019-10-18 01:16:14 -0400

    Part 2 Sub-Part 2

--- src/test/java/TodoBusinessImplMockTest.java
+++ src/test/java/TodoBusinessImplMockTest.java
@@ -3,9 +3,14 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import org.mockito.BDDMockito.*;
 
 class TodoBusinessImplMockTest {
 
@@ -21,5 +26,21 @@ class TodoBusinessImplMockTest {
         //if there are no results, Mockito will return and empty list
         List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
         assertEquals(2, filteredTodos.size());
+    }
+
+    @Test
+    void retrieveTodosRelatedToSpring_usingBDD() {
+        //get a mock of the TodoService class
+        TodoService todoServiceMock = mock(TodoService.class);
+        List<String> listMock = mock(List.class);
+        String actualResult;
+
+        given(listMock.get(anyInt())).willReturn("Spring");
+
+        //When
+        actualResult = listMock.get(0);
+
+        //Then
+        assertEquals(actualResult, "Spring");
     }
 }
